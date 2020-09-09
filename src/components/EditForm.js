ae0825c7625e99f708f7d1f89e2cbf73e92bf48d
Elaine <elaine@Felipes-iMac.local>
2020-09-09 14:18:09 -0500

    add edit form

---
+++ src/components/EditForm.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect} from 'react'
+import axios from 'axios';
+import { Redirect } from 'react-router-dom';
+const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL;
+
+const EditForm = (props) => {
+
+    let [long, setLong] = useState(props.user.longitude);
+    let [lat, setLat] = useState(props.user.latitude);
+    let [dis, setDis] = useState(props.user.radiusTrail);
+    let [redirect, setRedirect] = useState(false);
+
+    const handleLong = (e) =>{
+        setLong(e.target.value);
+    }
+    const handleLat = (e) =>{
+        setLat(e.target.value);
+    }
+    const handleDis = (e) =>{
+        setDis(e.target.value);
+    }
+
+    let handleSubmit = (e) => {
+        e.preventDefault();
+
+        const updateInfo = { long, lat, dis }
+
+        axios.post(`${REACT_APP_SERVER_URL}/api/users/profile`, updateInfo)
+        .then(response =>{
+            setRedirect(true);
+        })
+        .catch(error => console.log(error))
+    } 
+    if(redirect) return <Redirect to="/profile" />
+
+
+    
+        return(
+            <div className='edit-user'>
+                <h3>Edit your account</h3>
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor="Longitute">Longitude:</label>
+                        <input name="Latitude" value={long} onChange={handleLong}/>
+                    </div>
+                    <div>
+                        <label htmlFor="Latitude">Latitude:</label>
+                        <input name="Latitude" value={lat} onChange={handleLat}/>
+                    </div>
+                    <div>
+                        <label htmlFor="Distance">Distance:</label>
+                        <input name="Distance" value={dis} onChange={handleDis} />
+                    </div>
+                        <input type="hidden" name="id" value={props.user.id} />
+                    <button type="submit">Update</button>
+
+                </form>
+            </div>
+        )
+
+
+}
+
+export default EditForm;
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState} from 'react';
 import { Link } from 'react-router-dom';
+import EditForm from './EditForm';
 
 const Profile = (props) => {
+   let [currentForm, setForm] = useState("Edit User");
+
+
     console.log(props);
     const userData = props.user ? 
     (<div>
         <h1>Profile</h1>
         <p><strong>Name:</strong> {props.user.name}</p>
         <p><strong>Email:</strong>  {props.user.email}</p>
-        <p><strong>ID:</strong>  {props.user.id}</p>
+        <p className="id" name="id"><strong>ID:</strong>  {props.user.id}</p>
+        <p><strong>Longitude:</strong> {props.user.longitude}</p>
+        <p><strong>Latitude:</strong>  {props.user.latitude}</p>
+        <p><strong>Distance from Trails:</strong>  {props.user.radiusTrail}</p>
+
+        <input type ="button" onClick={() => ({currentForm}="Edit User") ? (setForm(currentForm = "Cancel"), console.log({currentForm})) : (setForm(currentForm="Edit User"), console.log({currentForm}))} value={currentForm}/>
     </div>) : <h4>Loading...</h4>
+
 
     const errorDiv = () => {
         return(
@@ -21,7 +31,11 @@ const Profile = (props) => {
 
     return(
         <div>
+            <div>
             {props.user ? userData : errorDiv}
+            </div>
+            <EditForm user={props.user} />
+            
         </div>
     )
 
